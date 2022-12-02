2b3f02a5aa3b9e16c62480588c0f7b4b5e0be3a2
anitagund <anitahk1992@gmail.com>
2022-12-02 09:58:41 +0530

    updated log4j

--- TripadvoisaryProject/src/main/java/in/Tripadvoisory/TestBase/TestBase.java
+++ TripadvoisaryProject/src/main/java/in/Tripadvoisory/TestBase/TestBase.java
@@ -1,22 +1,43 @@
 package in.Tripadvoisory.TestBase;
 
-	import java.time.Duration;
+import java.time.Duration;
 
-	import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.chrome.ChromeDriver;
-	import org.openqa.selenium.edge.EdgeDriver;
-	import org.openqa.selenium.firefox.FirefoxDriver;
-	import org.testng.annotations.AfterTest;
-	import org.testng.annotations.BeforeTest;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
-	import io.github.bonigarcia.wdm.WebDriverManager;
 
-	public class TestBase {
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class TestBase {
 	public static WebDriver driver;
-	@BeforeTest
-	public void setup()
+	public static Logger logger;
+		@BeforeClass
+		public void start()
+		{
+			logger=Logger.getLogger("Framework Tripadvoisary script");
+			PropertyConfigurator.configure("Log4j.properties");
+			logger.info("before class");
+		}
+		@AfterClass
+		public void end()
+		{
+			logger.info("after class");
+		}
+@Parameters("Browser")
+@BeforeMethod
+public void setup(String browser)
 	{
-		String browser="chrome";
+		
 		if(browser.equalsIgnoreCase("chrome"))
 		{
 			WebDriverManager.chromedriver().setup();
